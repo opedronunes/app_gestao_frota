b320f0f40ecda039fbd7bb6b6299497d18c8ef05
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-09-28 23:05:22 -0300

    feat: new creation user

--- login/LoginService.php
+++ login/LoginService.php
@@ -1,17 +1,49 @@
 <?php
 
+function inserirUsuario($conexao, $dados) {
+    
+    try {
+        
+        $query = "INSERT INTO tb_usuario (no_usuario, nu_cpf_usuario, email_usuario, senha_usuario, id_perfil, id_status) VALUES (:no_usuario, :nu_cpf_usuario, :email_usuario, :senha_usuario, :id_perfil, :id_status)";
+        $stmt = $conexao->prepare($query);
+        $stmt->bindParam(':no_usuario', $dados['no_usuario']);
+        $stmt->bindParam(':nu_cpf_usuario', $dados['nu_cpf_usuario']);
+        $stmt->bindParam(':email_usuario', $dados['email_usuario']);
+        $stmt->bindParam(':senha_usuario', $dados['senha_usuario']);
+        $stmt->bindParam(':id_perfil', $dados['id_perfil']);
+        $stmt->bindParam(':id_status', $dados['id_status']);
+
+        return $stmt->execute();
+
+        
+    } catch (\Throwable $e) {
+        var_dump($e->getMessage());
+    }
+}
+
+
+
+
+
+/*
 class LoginService {
 
     private $conexao;
-    private $usuario;
 
-    public function __construct(Conexao $conexao, $usuario)
+    public function __construct($conexao)
     {
         $this->conexao = $conexao->conectar();
-        $this->usuario = $usuario;
+    }
+
+    public function inserir() {
+        $query = "INSERT INTO tb_usuario (no_usuario, nu_cpf_usuario, email_usuario, senha_usuario, id_perfil, id_status) VALUES (:no_usuario, :nu_cpf_usuario, :email_usuario :senha_usuario, :id_perfil, :id_status)";
+        $stmt = $this->conexao->prepare($query);
+        
+        $stmt->bindParm(':no_usuario');
+        $stmt->execute();
     }
 
 }
-
+*/
 
 ?>
--- login/registro.php
+++ login/registro.php
@@ -1,105 +1,25 @@
 <?php
 
-require_once('../conexao/Conexao.php');
+require_once '../conexao/connection.php';
+require_once './LoginService.php';
 
- 
-// Defina variáveis e inicialize com valores vazios
-$email = $senha = $confirm_senha = "";
-$email_err = $senha_err = $confirm_senha_err = "";
- 
-// Processando dados do formulário quando o formulário é enviado
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Validar nome de usuário
-    if(empty(trim($_POST["nome"]))){
-        $username_err = "Por favor coloque um nome de usuário.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["nome"]))){
-        $username_err = "O nome de usuário pode conter apenas letras, números e sublinhados.";
-    } else{
-        // Prepare uma declaração selecionada
-        $sql = "SELECT id FROM tb_usuario WHERE email_usuario = :email";
-        
-        if($stmt = $conexao->prepare($sql)){
-            // Vincule as variáveis à instrução preparada como parâmetros
-            $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
-            
-            // Definir parâmetros
-            $param_email = trim($_POST["email"]);
-            
-            // Tente executar a declaração preparada
-            if($stmt->execute()){
-                if($stmt->rowCount() == 1){
-                    $username_err = "Este email de usuário já está em uso.";
-                } else{
-                    $email = trim($_POST["email"]);
-                }
-            } else{
-                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde.";
-            }
-
-            // Fechar declaração
-            unset($stmt);
-        }
-    }
+if (isset($_POST['nome']) && $_POST['cpf']) {
     
-    // Validar senha
-    if(empty(trim($_POST["senha"]))){
-        $senha_err = "Por favor insira uma senha.";     
-    } elseif(strlen(trim($_POST["senha"])) < 6){
-        $senha_err = "A senha deve ter pelo menos 6 caracteres.";
-    } else{
-        $senha = trim($_POST["senha"]);
+    $dados = [
+        'no_usuario' => $_POST['nome'],
+        'nu_cpf_usuario' => $_POST['cpf'],
+        'email_usuario' => $_POST['email'],
+        'senha_usuario' => $_POST['senha'],
+        'id_perfil' => $_POST['perfil'],
+        'id_status' => $_POST['status'],
+    ];
+    try {
+        inserirUsuario($conexao, $dados);
+        echo "<script>alert('Usuário inserido com sucesso!')</script>";
+    } catch (Exception $e) {
+        echo "<script>alert('Ocorreu um erro ao inserir o usuário, tente novamente mais tarde!')</script>";
+        echo '<script>alert("'.$e->getMessage().' Codigo erro: '.$e->getCode().'")</script>';
     }
-    
-    // Validar e confirmar a senha
-    if(empty(trim($_POST["confirm_senha"]))){
-        $confirm_senha_err = "Por favor, confirme a senha.";     
-    } else{
-        $confirm_senha = trim($_POST["confirm_senha"]);
-        if(empty($senha_err) && ($senha != $confirm_senha)){
-            $confirm_senha_err = "A senha não confere.";
-        }
-    }
-    
-    // Verifique os erros de entrada antes de inserir no banco de dados
-    if(empty($email_err) && empty($senha_err) && empty($confirm_senha_err)){
-        
-        // Prepare uma declaração de inserção
-        $sql = "INSERT INTO tb_usuario (no_usuario, nu_cpf_usuario, email_usuario, senha_usuario, id_perfil, id_status) VALUES (:nome, :cpf, :email :senha, :perfil, :status)";
-         
-        if($stmt = $conexao->prepare($sql)){
-            // Vincule as variáveis à instrução preparada como parâmetros
-            $stmt->bindParam(":nome", $param_nome, PDO::PARAM_STR);
-            $stmt->bindParam(":email", $param_cpf, PDO::PARAM_STR);
-            $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
-            $stmt->bindParam(":senha", $param_senha, PDO::PARAM_STR);
-            $stmt->bindParam(":perfil", $param_perfil, PDO::PARAM_STR);
-            $stmt->bindParam(":status", $param_status, PDO::PARAM_STR);
-            
-            // Definir parâmetros
-            $param_nome = $nome;
-            $param_cpf = $cpf;
-            $param_email = $email;
-            $param_senha = password_hash($senha, PASSWORD_DEFAULT); // Creates a password hash
-            $param_perfil = $perfil;
-            $param_status = $status;
-
-            
-            // Tente executar a declaração preparada
-            if($stmt->execute()){
-                // Redirecionar para a página de login
-                header("location: ../index.php");
-            } else{
-                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde.";
-            }
-
-            // Fechar declaração
-            unset($stmt);
-        }
-    }
-    
-    // Fechar conexão
-    unset($conexao);
 }
 
 ?>
@@ -118,7 +38,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="container-login">
                 <h2>Cadastro</h2>
                 <p>Por favor, preencha este formulário para criar uma conta.</p>
-                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <form action="./registro.php" method="post">
                     <div class="form-group">
                         <label class="form-label" for="nome">Nome:</label>
                         <input class="form-control" type="text" name="nome" id="nome" required>
@@ -131,7 +51,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
                     <div class="form-group">
                         <label>Email do usuário</label>
-                        <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                        <input type="email" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
                         <span class="invalid-feedback"><?php echo $email_err; ?></span>
                     </div> 
                     <div class="form-group">
@@ -139,18 +59,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                         <input type="password" name="senha" class="form-control <?php echo (!empty($senha_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $senha; ?>">
                         <span class="invalid-feedback"><?php echo $senha_err; ?></span>
                     </div>
+                    <!--
                     <div class="form-group">
                         <label>Confirme a senha</label>
                         <input type="password" name="confirm_senha" class="form-control <?php echo (!empty($confirm_senha_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_senha; ?>">
                         <span class="invalid-feedback"><?php echo $confirm_senha_err; ?></span>
                     </div>
+                    -->
                     
                     <div class="form-group">
                         <label class="form-label" for="perfil">Perfil:</label>
                         <select class="form-select" name="perfil" id="perfil" required>
-                            <option value="1">Adminstrador</option>
+                            <option value="1">Administrador</option>
                             <option value="2">Cliente</option>
-                            <option value="3">Fucionario</option>
+                            <option value="3">Funcionário</option>
                         </select>
                     </div>
 
