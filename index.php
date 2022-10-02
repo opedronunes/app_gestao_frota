7e0d63697ff756d3a422d8a32f084205c799177e
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-10-01 23:59:05 -0300

    feat: increments css in page login

--- index.php
+++ index.php
@@ -131,36 +131,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./assets/styles/bootstrapv5.2.min.css">
-    <link rel="stylesheet" type="text/css" href="/login.css">
+    <link rel="stylesheet" type="text/css" href="/assets/styles/index.css">
+
     <title>Login - App Gestão de Frotas</title>
 </head>
 <body>
     <main>
-        <div class="container">
-            <h2>Login</h2>
-            <?php
-                if (!empty($login_err)) {
-                    echo "<div class='alert alert-danger'>" .$login_err. "</div>";
-                }
-            ?>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <div class="form-group">
-                    <label for="name">Email do usuário:</label>
-                    <input type="text" name="email" id="name" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
-                    <span class="invalid-feedback"><?php echo $email_err; ?></span>
-                </div>
-                <div>
-                    <label for="password">Senha:</label>
-                    <input type="password" name="senha" id="senha" class="form-control <?php echo (!empty($senha_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $senha ?>">
-                    <span class="invalid-feedback"><?php echo $senha_err ?></span>
-                </div>
-                <div class="form-group">
-                    <input type="submit" value="Entrar" class="btn btn-outline-success mt-2">
-                </div>
-                <p>Não tem uma conta? <a href="./login/registro.php">fazer cadastro</a>.</p>
-            </form>
-        </div>
+        <h2>Acessar Dashboard</h2>
+        <?php
+            if (!empty($login_err)) {
+                echo "<div class='alert alert-danger'>" .$login_err. "</div>";
+            }
+        ?>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="inputs">
+                <label for="name">Email do usuário:</label>
+                <input type="text" name="email" id="name" class="<?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                <span class="invalid-feedback"><?php echo $email_err; ?></span>
+            </div>
+            <div class="inputs">
+                <label for="password">Senha:</label>
+                <input type="password" name="senha" id="senha" class="<?php echo (!empty($senha_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $senha ?>">
+                <span class="invalid-feedback"><?php echo $senha_err ?></span>
+            </div>
+            <input type="submit" value="Entrar">
+            <p>Não tem uma conta? <a href="./login/registro.php">fazer cadastro</a>.</p>
+        </form>
     </main>
 
     <script src="./assets/js/bootstrapv5.2.min.js"></script>
