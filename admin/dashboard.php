410ee321c9d9c72be5a00f704a5fb831729de031
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-10-02 23:45:47 -0300

    feat: update layout dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -13,10 +13,124 @@ if ((!isset($_SESSION['id_usuario'])) AND (!isset($_SESSION['email_usuario']))) 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/phosphor-icons"></script>
+    <link rel="stylesheet" href="../assets/styles/index.css">
+    <link rel="stylesheet" href="../assets/styles/bootstrapv5.2.min.css">
     <title>Dashboard</title>
 </head>
 <body>
-    <h1>Bem vindo <?= $_SESSION['no_status'] ?>!</h1>
-    <a href="../login/logoff.php">sair</a>
+    
+    <header>
+        <h1>App Getsão de Frotas</h1>
+    </header>
+    <div id="dashboard">
+        <div class="row g-0">
+            <!--Menu lateral-->
+            <div class="col-md-2">
+                <nav id="menu-lateral">
+                    <ul id="menu">
+                        <li><a href=""><i class="ph-house"></i> Home</a></li>
+                        <li><a href=""><i class="ph-currency-dollar"></i> Faturameto</a></li>
+                        <li><a href=""><i class="ph-truck"></i> Frotas</a></li>
+                        <li><a href=""><i class="ph-users-three"></i> Funcionários</a></li>
+                    </ul>
+                    <div id="perfil">
+                        <div id="user">
+                            <button title="Editar Perfil"><i class="ph-user"></i></button>
+                            <p><?= $_SESSION['no_usuario'] ?></p>
+                        </div>
+                        <a href="../login/logoff.php"><i class="ph-sign-out" title="Sair"></i></a>
+                    </div>
+                </nav>
+                
+            </div>
+            <!--Conteúdo central-->
+            <div class="col-md-10">
+                <div id="content-dashboard">
+
+                    <div class="row row-cols-1 row-cols-md-2 g-2">
+                        <div class="col">
+                            <div class="card border-primary mb-3 bg-transparent">
+                                <div class="card-header">Faturamento</div>
+                                <div class="card-body text-white">
+                                    <p>Total Aberto = R$ 1.000,00</p>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card border-warning mb-3 bg-transparent">
+                                <div class="card-header">Frota</div>
+                                <div class="card-body text-white">
+                                    <p>Total Aberto = R$ 1.000,00</p>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card border-success mb-3 bg-transparent">
+                                <div class="card-header">Funcionarios</div>
+                                <div class="card-body text-white">
+                                    <p>Total Aberto = R$ 1.000,00</p>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                    
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!--
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Offcanvas navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                    </a>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                </ul>
+                <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+            </div>
+        </div>
+    </nav>
+    -->
+    <script src="../assets/js/bootstrapv5.2.min.js"></script>
 </body>
 </html>
